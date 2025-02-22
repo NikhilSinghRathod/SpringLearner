8cb94761258ec53f52683d678427d57098d4ea29
NikhilSinghRathod <r7445580@gmail.com>
2025-02-22 11:49:01 +0530

    [Nikhil Singh Rathode] Add.  REST API Hello from BridgeLabz

--- src/main/java/com/spring/SpringLearner/controller/HelloRestController.java
+++ src/main/java/com/spring/SpringLearner/controller/HelloRestController.java
@@ -1,13 +1,16 @@
 package com.spring.SpringLearner.controller;
 
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloRestController {
-    //curl localhost:8080/hello -w "\n"
-    @RequestMapping("/home")
-    public String sayHello(){
-        return "Hello From Bridgelabz";
+
+    @GetMapping
+    public String sayHello() {
+        return "Hello from BridgeLabz!";
     }
 }
